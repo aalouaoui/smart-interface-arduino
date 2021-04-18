6d2bfa295908e21f47a5171f9a8f4c831096fa04
aalouaoui <khalidaalouaoui@gmail.com>
2021-04-18 02:58:14 +0000

    AnalogButtons without hardcoding

--- AnalogButtons.h
+++ AnalogButtons.h
@@ -5,8 +5,30 @@
 class AnalogButtons
 {
 public:
-    int lastButtonState;
-    int getState();
+    int lastButtonState = 0;
+    int pin = A0;
+    int valuesCount = 5;
+    int values[5] = {500, 1200, 2500, 3400, 4800};
+    int valueEpsilon = 200;
+
+    int getState()
+    {
+        int buttonState = 0;
+        int value = map(analogRead(pin), 0, 1023, 0, 5000);
+
+        for (int i = 0; i < valuesCount; i++)
+        {
+            if (value > values[i] - valueEpsilon && value < values[i] + valueEpsilon)
+            {
+                buttonState = i + 1;
+                break;
+            }
+        }
+
+        int returnValue = buttonState != 0 && lastButtonState == 0 ? buttonState : 0;
+        lastButtonState = buttonState;
+        return returnValue;
+    }
 };
 
 #endif
