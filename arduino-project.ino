1115a5dfaf6bb2ef024d9fae4a4e11e8925c262b
aalouaoui <khalidaalouaoui@gmail.com>
2021-04-18 02:33:27 +0000

    Shorter AnalogButtons

--- arduino-project.ino
+++ arduino-project.ino
@@ -1,31 +1,23 @@
 class AnalogButtons
 {
-private:
+public:
     int lastButtonState;
-
-    int getCurrentState()
+    int getState()
     {
-        // 0.5V 1.2V 2.5V 3.4V 4.9V
+        int buttonState = 0;
         int value = map(analogRead(A0), 0, 1023, 0, 5000);
 
         if (value > 300 && value < 700)
-            return 1;
+            buttonState = 1;
         if (value > 1000 && value < 1400)
-            return 2;
+            buttonState = 2;
         if (value > 2300 && value < 2700)
-            return 3;
+            buttonState = 3;
         if (value > 3200 && value < 3600)
-            return 4;
+            buttonState = 4;
         if (value > 4600)
-            return 5;
+            buttonState = 5;
 
-        return 0;
-    }
-
-public:
-    int getButton()
-    {
-        int buttonState = getCurrentState();
         int returnValue = 0;
         if (buttonState != 0 && lastButtonState == 0)
             returnValue = buttonState;
@@ -45,7 +37,7 @@ void setup()
 void loop()
 {
     // Input Handling
-    int button = analogButtons.getButton();
+    int button = analogButtons.getState();
     if (button != 0)
         Serial.println(button);
     delay(10);
