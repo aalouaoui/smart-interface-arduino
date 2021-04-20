1b84bd07fe5ff796ea0533315a9ce90c9335079b
aalouaoui <khalidaalouaoui@gmail.com>
2021-04-20 17:15:18 +0000

    Fixed unhandled case on AnalogButton

--- AnalogButtons.h
+++ AnalogButtons.h
@@ -9,21 +9,22 @@ public:
     int lastButtonState = 0;
     int getState()
     {
-        int buttonState = 0;
         int value = map(analogRead(BUTTONS_PIN), 0, 1023, 0, 5000);
 
-        for (int i = 0; i < BUTTONS_COUNT; i++)
+        int buttonState = value < BUTTONS_EPSILON ? 0 : -1;
+
+        for (int i = 0; i < BUTTONS_COUNT && buttonState == -1; i++)
         {
             if (value > (BUTTONS_ARRAY[i] - BUTTONS_EPSILON) && value < (BUTTONS_ARRAY[i] + BUTTONS_EPSILON))
-            {
                 buttonState = i + 1;
-                break;
-            }
         }
 
-        int returnValue = buttonState != 0 && lastButtonState == 0 ? buttonState : 0;
+        if (buttonState == -1)
+            return 0;
+
+        bool isRisingEdge = lastButtonState == 0 && buttonState != 0;
         lastButtonState = buttonState;
-        return returnValue;
+        return isRisingEdge ? buttonState : 0;
     }
 };
 
